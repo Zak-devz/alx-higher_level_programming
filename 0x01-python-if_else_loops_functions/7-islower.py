5b6ec663632ca34eedeb76111dc4e797bf1d772b
root <zakariarassili@gmail.com>
2023-06-06 18:40:40 +0000

    corrected the code, delete print lines

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     if ord(c) >= 97 or ord(c) < 123:
-        print("{} is lower".format(c))
         return True
     else:
-        print("{} is upper".format(c))
         return False
-
