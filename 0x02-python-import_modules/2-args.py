d024d81c04efb37631f71b522433b48e329fb30f
root <zakariarassili@gmail.com>
2023-06-08 13:12:54 +0000

    corrected case for argv =1, and update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-x = len(argv) - 1
+if __name__ == "__main__":
+    import sys
+
+x = len(sys.argv) - 1
 if x == 0:
     print("0 arguments.")
+elif x == 1:
+    print("1 argument:")
+    print("{}: {}".format(x, sys.argv[x]))
 else:
     print("{} arguments:".format(x))
-for i in range(1, x + 1):
-        print("{}: {}".format(i, argv[i]))
+for i in range(x):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
