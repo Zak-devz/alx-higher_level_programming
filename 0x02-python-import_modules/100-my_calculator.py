c27ea28180037c4fe5345a8f2d364e872c9ae0fe
root <zakariarassili@gmail.com>
2023-06-08 19:49:13 +0000

    corrected ops

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif (operator == '-'):
-        print("{} - {} = {}".format(a, b, calulator_1.sub(a, b)))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif (operator == '*'):
-        print("{} * {} = {}".format(a, b, calulator_1.mul(a, b)))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     elif (operator == '/'):
-        print("{} / {} = {}".format(a, b, calulator_1.div(a, b)))
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
