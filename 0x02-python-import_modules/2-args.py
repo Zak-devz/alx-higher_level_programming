91e1a6aa17d2085f32daed9d5f36eabd5a69c79c
root <zakariarassili@gmail.com>
2023-06-08 13:15:44 +0000

    correct indent

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 if __name__ == "__main__":
     import sys
 
-x = len(sys.argv) - 1
-if x == 0:
-    print("0 arguments.")
-elif x == 1:
-    print("1 argument:")
-    print("{}: {}".format(x, sys.argv[x]))
-else:
-    print("{} arguments:".format(x))
-for i in range(x):
-    print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    x = len(sys.argv) - 1
+    if x == 0:
+        print("0 arguments.")
+    elif x == 1:
+        print("1 argument:")
+        print("{}: {}".format(x, sys.argv[x]))
+    else:
+        print("{} arguments:".format(x))
+    for i in range(x):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
