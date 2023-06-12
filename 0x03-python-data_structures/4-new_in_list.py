35aa2f26356cbeb9724564496ff569c486ecda5c
root <zakariarassili@gmail.com>
2023-06-12 17:46:54 +0000

    add variable for new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new = my_list.cocpy()
     if idx < 0 or idx > len(my_list) - 1:
         return my_list.copy()
     else:
-        my_list[idx] = element
-        return my_list
+        new[idx] = element
+        return new
