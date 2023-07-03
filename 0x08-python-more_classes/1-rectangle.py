81b33f5f356781f237b05b13409bcd50274e6b53
root <zakariarassili@gmail.com>
2023-07-03 14:47:15 +0000

    corrected indentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,28 +9,28 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """width of the rec"""
-            return self.__width
+    @property
+    def width(self):
+        """width of the rec"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-        @property
-        def height(self):
-            """height of the rectangle."""
-            return self.__height
+    @property
+    def height(self):
+        """height of the rectangle."""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
