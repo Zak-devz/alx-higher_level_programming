fe54443739def35414f29cb600962aaebd77ef14
root <zakariarassili@gmail.com>
2023-08-17 20:53:40 +0000

    task 8 done

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,4 @@
--- Create database hbtn_0d_usa.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Create cities table.
-USE hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS cities (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id),
-	name VARCHAR(256) NOT NULL
-	);
+-- List all the cities of California in database hbtn_0d_usa.
+SELECT id, name FROM cities WHERE state_id IN (
+	SELECT id FROM states WHERE name = "California")
+ORDER BY id;
