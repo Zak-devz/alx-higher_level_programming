9ffd0e92544d0b34c901fd6831055133d76b21bf
root <zakariarassili@gmail.com>
2023-09-11 15:18:12 +0000

    task 2 corrected case 2 exacts args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length > 2) {
+if (process.argv.length === 2) {
+  console.log('No arguments');
+} else if (process.argv.length === 3) {
   console.log('Argument found');
-} else {
-  console.log('No argument');
+}
+else {
+  console.log('Arguments found');
 }
