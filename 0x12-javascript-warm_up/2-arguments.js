ad5738f0b401db5e2f1d0c18c10d1966ee3d85d0
root <zakariarassili@gmail.com>
2023-09-11 15:21:37 +0000

    correct semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,6 @@ if (process.argv.length === 2) {
   console.log('No arguments');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
-}
-else {
+} else {
   console.log('Arguments found');
 }
