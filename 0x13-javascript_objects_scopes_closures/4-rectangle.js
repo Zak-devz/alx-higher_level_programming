b43c324bbd0ba47de3897f3bbd8de18e3eab4716
root <zakariarassili@gmail.com>
2023-09-12 17:46:01 +0000

    correct methods

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -14,14 +14,12 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    for (let i = 0; i < this.width; i++) {
-      console.log('X'.repeat(this.height));
-    }
+    this.width = this.height;
+    this.height = this.width;
   }
 
   double () {
-    for (let i = 0; i < this.width * 2; i++) {
-      console.log('X'.repeat(this.height * 2));
-    }
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 };
