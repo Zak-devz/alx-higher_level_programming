83acbd28a13e00e40e322bbc2270a11b81197a1e
root <zakariarassili@gmail.com>
2023-09-12 18:57:13 +0000

    task 6 coompleted

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const Sqr = require('./5-square.js');
+module.exports = class Square extends Sqr {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};
