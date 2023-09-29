14dc7e2f0468cc578e8dc1051c08b0b7b58766ce
root <zakariarassili@gmail.com>
2023-09-29 14:10:25 +0000

    corrected indentation

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,10 +8,10 @@ import requests
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+            sys.argv[2], sys.argv[1])
+    res = requests.get(url)
+    commits = res.json()
 
-    response = requests.get(url)
-    commits = response.json()
     try:
         for i in range(10):
             print("{}: {}".format(
