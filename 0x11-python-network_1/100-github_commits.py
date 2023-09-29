8fd7b708ff3e5ba34a6e45238e758d2d525efa05
root <zakariarassili@gmail.com>
2023-09-29 14:22:13 +0000

    corrected  the code and added new feat

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-Usage: ./100-github_commits.py <repository name> <repository owner>
-"""
+"""A script to list the 10 most recent commits on a GitHub repository."""
 import sys
 import requests
 
 
-if __name__ == "__main__":
+def main():
+    """Main function to fetch and display commits."""
+    if len(sys.argv) != 3:
+        print("Usage: ./script.py <repository name> <repository owner>")
+        sys.exit(1)
+
     repo = sys.argv[1]
     owner = sys.argv[2]
     limit = 10
-    url = f'https://api.github.com/repos\
-            /{repo}/{owner}/commits?per_page={limit}')
-    
-    res = requests.get(url).json()
-    for commit in response:
-        name = commit.get("commit").get("author").get("name")
-        print(f'{commit.get("sha")}: {name}')
+    url = (
+        f'https://api.github.com/repos/{repo}/{owner}/commits'
+        f'?per_page={limit}'
+    )
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        commits = response.json()
+        for commit in commits:
+            name = commit.get("commit").get("author").get("name")
+            print(f'{commit.get("sha")}: {name}')
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
