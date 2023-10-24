eef185a79920ab792ff0f1a89402a5fbf623d8c7
Zak-devz <zakariarassili@gmail.com>
2023-10-24 15:23:39 +0100

    fix err poblem

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,11 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 const request = require('request');
 
 let url = process.argv[2];
 
-request(url, (response) => {
+request(url, (err, response) => {
+  if (err) {
+    console.log(err);
+  }
   console.log(`code: ${response.statusCode}`);
 });
